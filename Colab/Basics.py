919bf2b361a4c6a286abacbe9aca2d0602137c63
t-xim <46313127+t-xim@users.noreply.github.com>
2019-04-03 13:56:54 +1100

    Create Basics.py

---
+++ Colab/Basics.py
@@ -0,0 +1,8 @@
+# Google colab things
+
+# Importing and mounting
+from google.colab import drive
+drive.mount('/content/gdrive')
+
+# Setting the drive
+cd /content/gdrive/My Drive/42028-DL-CNN/Week 2: Lab-1/OpenCV/dataset
