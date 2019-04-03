1261e53fd62d023b2e6829e29ed161e3929c6cf6
t-xim <46313127+t-xim@users.noreply.github.com>
2019-04-03 13:57:47 +1100

    Create OpenCV.py

---
+++ Image-Recognition/OpenCV.py
@@ -0,0 +1,18 @@
+# import the packages required 
+# CV2 is the package used for image processing and computer vision tasks
+import cv2    
+
+# matplotlib is plotting library used for displaying images
+import matplotlib.pyplot as plt  
+
+# Numpy is for matrix computation
+import numpy as np
+from IPython.display import display, HTML, clear_output
+
+# Which version of OpenCV is used?
+print(cv2.__version__)
+
+
+
+# How to use cv2 package 
+import cv2
