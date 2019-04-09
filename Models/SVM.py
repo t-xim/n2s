ed07b6d501765ae92c07a3d5f8b085b49ac223a7
t-xim <46313127+t-xim@users.noreply.github.com>
2019-04-09 21:03:49 +1000

    Update SVM.py

--- Models/SVM.py
+++ Models/SVM.py
@@ -1,4 +1,5 @@
 # Assuming X_train and y_train already input.
+# Assuming X_test and y_train already input.
 
 from sklearn import svm
 
@@ -7,4 +8,15 @@ model = svm.SVC(kernel = "rbf", C = 100.0, random_state = 322)
 model.fit(X_train, y_train)
 
 # Accuracy
-format(model.score(X_train, y_train)
+model.score(X_train, y_train)
+
+# Predicting (Not sure if looping in necessary, taken from image processing)
+predictions = []
+
+for i in range(len(X_test)):
+  to_predict = X_test[i]
+  prediction = model.predict(to_predict)
+  predictions.append(prediction);
+
+# Accuracy
+metric.accuracy_score(y_test, predictions)
