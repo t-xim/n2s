ec5d4888d1c52b6903914f543680d7839882e4c4
t-xim <46313127+t-xim@users.noreply.github.com>
2019-04-26 09:13:27 +1000

    Create dup-row-to-col.py

---
+++ Python/dup-row-to-col.py
@@ -0,0 +1,22 @@
+# train data that gets deleted
+temp_train = train
+
+# First loop
+cur_train = temp_train.sort_values("trajectory_id").drop_duplicates("hash")
+X = cur_train
+temp_train = temp_train[~temp_train.isin(cur_train)]
+
+X = X.add_suffix('_1')
+X = X.rename(columns = {"hash_1" : "hash"})
+
+max_hash_count = max(list(train.groupby("hash").size()))
+
+
+# Other loops
+for col_index in range(max_hash_count - 1):
+    cur_train = temp_train.sort_values("trajectory_id").drop_duplicates("hash")
+    X = X.merge(cur_train, how = "left", on = "hash", suffixes = ("", "_" + str(col_index + 1)))
+    temp_train = temp_train[~temp_train.isin(cur_train)]
+
+print(len(X))
+X.head()
